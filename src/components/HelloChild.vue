718da8a78ae7b4ad3fe0ca8d11bced8a1a755c26
Connor Frendt <connor.frendt@pollinate.com>
2019-10-17 09:14:20 -0700

    fixed links

--- src/components/HelloChild.vue
+++ src/components/HelloChild.vue
@@ -2,8 +2,8 @@
   <div>
     <p>This is the Hello Child component!</p>
     <div>
-      <a href="'https://linkedin.com/connorfrendt'"><img src="../assets/linkedin.png" style="width: 50px" /></a>
-      <a href="https://github.com/connorfrendt"><img src="../assets/github.jpg" style="width: 50px" /></a>
+      <a href="https://www.linkedin.com/in/connorfrendt/"><img src="../assets/linkedin.png" style="width: 50px" /></a>
+      <a href="https://www.github.com/connorfrendt"><img src="../assets/github.jpg" style="width: 50px" /></a>
     </div>
   </div>
 </template>
