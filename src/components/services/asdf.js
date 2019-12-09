5406a82c58321223657b49a9c7f5ff786f75865e
Connor Frendt <connor.frendt@pollinate.com>
2019-12-09 15:40:58 -0800

    removed unwanted files

--- src/components/services/asdf.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-    url: 'https://us.battle.net/oauth/token',
-    beforeSend: function(xhr) {
-         xhr.setRequestHeader("Authorization", "Bearer 6QXNMEMFHNY4FJ5ELNFMP5KRW52WFXN5")
-    }, success: function(data){
-        alert(data);
-    }
-})
